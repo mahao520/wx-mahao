53443417daf889cc30e44c36c4cd0584c8eba8bc
mahao520 <2511793751@qq.com>
2022-10-11 19:48:23 +0800

    hotfix: urls

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from wxcloudrun import views
 from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = (
     # 计数器接口
@@ -24,5 +25,5 @@ urlpatterns = (
     # 获取主页
     url(r'(/)?$', views.index),
 
-    url('echo', views.echo),
+    path('echo', views.echo),
 )
